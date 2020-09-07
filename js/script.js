694f5e88ceed3950f91aae7f35103f0df70c954b
marc sitze <marcsitze01@gmail.com>
2020-09-07 14:43:29 +0100

    payement and other edits

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+  /* =========================================
+                Clients
+  ============================================ */
+  console.log('This is working');
+  $(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items: 2,
+    autoplay: false,
+    nav: true,
+    smartSpeed: 700,
+    loop: true,
+    dots: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    responsive: {
+        // breakpoint from 0 up
+        0: {
+            items: 2
+        },
+        // breakpoint from 480 up
+        480: {
+            items: 2
+        },
+        // breakpoint from 768 up
+        768: {
+            items: 2
+        }
+    }
+  });
+
+
+});
